ff558f56bcbb0e8ce7a6fca99deb6fa6bf23e2cb
nipajesmin <nipakterjesmin@gmail.com>
2024-12-23 22:24:32 +0600

    footer added

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../sharePages/Navbar';
+import Footer from '../sharePages/Footer';
 
 const MainLayout = () => {
     return (
         <div className='max-w-7xl mx-auto'>
             <Navbar></Navbar>
             <Outlet></Outlet>
-            
+            <Footer></Footer>
         </div>
     );
 };
